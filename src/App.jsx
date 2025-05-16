9b4e75b9b8ffb52ecd2d680da1e53ec318800e1b
Dmitrii <marutkin.dmitriy@gmail.com>
2025-05-16 18:46:31 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import Card from './Components/Card';
 import { motion } from 'framer-motion';
 import clsx from 'clsx';
 
+import img1 from './assets/1.png'
+import img2 from './assets/2.png'
+import img3 from './assets/3.png'
+import img4 from './assets/4.png'
+import img5 from './assets/5.png'
+
 const App = () => {
   const cards = [
-    { id: 1, date: '2025-05-01', image: '/images/1.png' },
-    { id: 2, date: '2025-05-11', image: '/images/2.png' },
-    { id: 3, date: '2025-05-15', image: '/images/3.png' },
-    { id: 4, date: '2025-06-17', image: '/images/4.png' },
-    { id: 5, date: '2025-06-19', image: '/images/5.png' },
+    { id: 1, date: '2025-05-01', image: img1 },
+    { id: 2, date: '2025-05-11', image: img2 },
+    { id: 3, date: '2025-05-15', image: img3 },
+    { id: 4, date: '2025-06-17', image: img4 },
+    { id: 5, date: '2025-06-19', image: img5 },
   ];
 
   return (
